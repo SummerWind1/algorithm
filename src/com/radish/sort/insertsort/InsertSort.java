0d5bb0b56db8c8d7e083b24298990c1c61f02d48
fangpan <fangpan@3vjia.com>
2021-08-23 18:15:02 +0800

    [Feature] 插入排序

--- src/com/radish/sort/insertsort/InsertSort.java
+++ src/com/radish/sort/insertsort/InsertSort.java
@@ -3,6 +3,10 @@ package com.radish.sort.insertsort;
 /**
  * 插入排序, 类似于扑克牌，每次选定一个位置，然后保证位置左右的扑克牌是有序的.可以依赖递归实现
  *
+ * 这个算法的主要判断难度在于如何将一个元素放入有序数组中，还能保证数组的有序性
+ *
+ * TODO 回头看看小灰的算法是怎么实现的
+ *
  * @author fangp fangp@3vjia.com
  * @since 2021-08-20 15:20
  */
@@ -14,7 +18,6 @@ public class InsertSort {
 
         // 外层循环控制比较的轮数, 最后一个数不需要比较
         for (int i = 0; i < array.length -1; i++) {
-
             // 内层循环控制每轮的比较, 每一轮都要和已经排好序的数组最后一个元素进行比较，然后判断是否需要进行位置交换
             for (int j = i+1; j > 0; j--) {
                 if (array[j] < array[j-1]) {
--- src/com/radish/sort/quicksort/QuickSort.java
+++ src/com/radish/sort/quicksort/QuickSort.java
@@ -3,6 +3,12 @@ package com.radish.sort.quicksort;
 /**
  * 快速排序
  *
+ * 双路快速排序法
+ * 1、选取一个坐标值 pivot
+ * 2、选取数组的第一个元素下标和最后一个元素的下标作为指针
+ * 3、然后
+ *
+ *
  * @author fangp fangp@3vjia.com
  * @since 2021-08-20 15:19
  */
@@ -12,12 +18,19 @@ public class QuickSort {
         // 这个排序我忘记定义是什么了。。。
         int [] array = new int[] {1, 8, 4, 3, 6, 7, 2, 5};
 
-        // 有双边循环和单边循环两种处理方式来解决快速排序
+        // 有双边循环和单边循环两种处理方式来解决快速排序、还有三路快速排序法
 
 
 
     }
 
+    /**
+     * 1、双边循环
+     * @param array
+     */
+    public static void partition(int[] array) {
 
+
+    }
 
 }
