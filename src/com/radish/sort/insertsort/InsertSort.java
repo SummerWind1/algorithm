8299e0126b67928b5fb86e961ee2c6d1d96dbe54
panfang <panfang@kugou.net>
2022-07-12 11:59:30 +0800

    [Feature] 排序复习二分查找、冒泡排序、插入排序、选择排序；时间复杂度O(n2),不需要额外的内存，都是基于比较进行排序

--- src/com/radish/sort/insertsort/InsertSort.java
+++ src/com/radish/sort/insertsort/InsertSort.java
@@ -1,5 +1,7 @@
 package com.radish.sort.insertsort;
 
+import java.util.Arrays;
+
 /**
  * 插入排序, 类似于扑克牌，每次选定一个位置，然后保证位置左右的扑克牌是有序的.可以依赖递归实现
  *
@@ -16,26 +18,52 @@ public class InsertSort {
 
         int [] array = new int[] {1, 8, 4, 3, 6, 7, 2, 5};
 
-        // 外层循环控制比较的轮数, 最后一个数不需要比较
-        for (int i = 0; i < array.length -1; i++) {
-            // 内层循环控制每轮的比较, 每一轮都要和已经排好序的数组最后一个元素进行比较，然后判断是否需要进行位置交换
-            for (int j = i+1; j > 0; j--) {
-                if (array[j] < array[j-1]) {
-                    // 需要进行位置互换, 如何保证有序区间添加新元素之后继续有序(for 循环将元素换到指定位置)
-                    int temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
+//        // 外层循环控制比较的轮数, 最后一个数不需要比较
+//        for (int i = 0; i < array.length -1; i++) {
+//            // 内层循环控制每轮的比较, 每一轮都要和已经排好序的数组最后一个元素进行比较，然后判断是否需要进行位置交换
+//            for (int j = i+1; j > 0; j--) {
+//                if (array[j] < array[j-1]) {
+//                    // 需要进行位置互换, 如何保证有序区间添加新元素之后继续有序(for 循环将元素换到指定位置)
+//                    int temp = array[j];
+//                    array[j] = array[j-1];
+//                    array[j-1] = temp;
+//                } else {
+//                    // 此轮结束
+//                    break;
+//                }
+//            }
+//            System.out.println(Arrays.toString(array));
+//        }
+//        // 打印
+//        System.out.println(Arrays.toString(array));
+        insertSort(array);
+
+
+    }
+
+
+    /**
+     * 插入排序； 每次定义一个有序区间，把一个只逐个比较，然后交换位置
+     */
+    public static void insertSort(int [] array) {
+
+        // 最外层循环控制比较次数
+        for (int i = 0; i < array.length - 1; i++) {
+
+            // 内层循环控制比较, 内存for循环结束之后，数组左边会维护一个从小到大的数组，每次都从原始数组中多取一个元素和有序数组中的元素进行比较，然后交换位置
+            for (int j = i + 1; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    // 交换一下位置
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
                 } else {
-                    // 此轮结束
                     break;
                 }
             }
+            System.out.println(Arrays.toString(array));
         }
-        // 打印
-        for (int i1 : array) {
-            System.out.println(i1);
-        }
-
+        System.out.println(Arrays.toString(array));
     }
 
 }
--- src/com/radish/sort/selectsort/SelectSort.java
+++ src/com/radish/sort/selectsort/SelectSort.java
@@ -1,5 +1,7 @@
 package com.radish.sort.selectsort;
 
+import java.util.Arrays;
+
 /**
  * 选择排序， 给定一个数组，随机选取其中的一个元素作为最小值，然后遍历比较是否有比选中最小值还小的元素，并确定元素的位置，
  * 然后将遍历得到的最小值和选中的最小值进行位置交换
@@ -13,7 +15,8 @@ package com.radish.sort.selectsort;
  * @author fangp fangp@3vjia.com
  * @since 2021-08-20 15:21
  *
- * TODO 看看小灰书里是怎么实现的
+ * 选择排序
+ * 属于原地排序，时间复杂度O(n2) 假定每次把数组的第一个元素作为最小值，在内存循换中用剩余的元素和假定最小值进行比较； 如果遇到比最小值小的值，最小值下标更新一下，每次比较都会和最新的最小值进行比较
  *
  */
 public class SelectSort {
@@ -21,30 +24,56 @@ public class SelectSort {
     public static void main(String[] args) {
 
         int [] array = new int[] {1, 8, 4, 3, 6, 7, 2, 5};
+//
+//        // 外层循环控制数组的比较轮数(eg: 8个元素的数组, 第一轮需要比较7次 , 8个元素，需要进行6轮比较)
+//        for (int i = 0; i < array.length - 1; i++) {
+//
+//            int min = i;
+//            // 选中第一个元素为最小值
+//            // 内存循环控制每轮数据之间的比较, 内层for循环每一次取一个元素和最小元素进行比较，获取到比较得到的最小元素的位置
+//            for (int j = i+1; j < array.length; j++) {
+//                if (array[j] < array[min]) {
+//                    min = j;
+//                }
+//            }
+//
+//            // 说明最小值发生了变更
+//            if (i != min) {
+//                int tmp = array[i];
+//                array[i] = array[min];
+//                array[min] = tmp;
+//            }
+//
+//            System.out.println(Arrays.toString(array));
+//        }
+//        // 打印一下
+//        System.out.print(Arrays.toString(array));
 
-        // 外层循环控制数组的比较轮数(eg: 8个元素的数组, 第一轮需要比较7次 , 8个元素，需要进行6轮比较)
+        selectSort(array);
+
+    }
+
+
+    public static void selectSort(int [] array) {
+
         for (int i = 0; i < array.length - 1; i++) {
-
-            int min = i;
-            // 选中第一个元素为最小值
-            // 内存循环控制每轮数据之间的比较, 内层for循环每一次取一个元素和最小元素进行比较，获取到比较得到的最小元素的位置
+            // 当前元素未最小值
+            int min_index = i;
             for (int j = i+1; j < array.length; j++) {
-                if (array[j] < array[min]) {
-                    min = j;
+                // 从数组的第二个元素开始比较，最后一个元素不需要比较
+                if (array[j] < array[min_index]) {
+                    min_index = j;
                 }
             }
-
-            // 说明最小值发生了变更
-            if (i != min) {
-                int tmp = array[i];
-                array[i] = array[min];
-                array[min] = tmp;
+            // 位置交换
+            if (min_index != i) {
+                int tmp = array[min_index];
+                array[min_index] = array[i];
+                array[i] = tmp;
             }
-
+            System.out.println(Arrays.toString(array));
         }
-        // 打印一下
-        for (int i1 : array) {
-            System.out.println(i1);
-        }
+        System.out.print(Arrays.toString(array));
     }
+
 }
